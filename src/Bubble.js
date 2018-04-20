ac4be71ba0c923233fb01d1110e529e1ef862585
Adriel Fabella <adriel.fab@gmail.com>
2018-04-19 22:53:10 -0400

    [Update] Radius for both sides

--- src/Bubble.js
+++ src/Bubble.js
@@ -179,7 +179,9 @@ const styles = {
       alignItems: 'flex-start',
     },
     wrapper: {
-      borderRadius: 15,
+      borderTopRightRadius: 15,
+      borderTopLeftRadius: 15,
+      borderBottomRightRadius: 15,
       backgroundColor: Color.leftBubbleBackground,
       marginRight: 60,
       minHeight: 20,
@@ -200,7 +202,7 @@ const styles = {
     wrapper: {
       borderTopRightRadius: 15,
       borderTopLeftRadius: 15,
-      borderBottomRightRadius: 15,
+      borderBottomLeftRadius: 15,
       backgroundColor: Color.defaultBlue,
       marginLeft: 60,
       minHeight: 20,
